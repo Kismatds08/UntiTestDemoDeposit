79922e9390774e9d070d3df7e31d8b39c9d000b7
Kismat Shrestha <kismatds08@gmail.com>
2020-09-02 21:34:36 -0500

    Spring

--- src/test/java/com/s3/com/demo/DemoApplicationTests.java
+++ src/test/java/com/s3/com/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.s3.com.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,6 +11,15 @@ class DemoApplicationTests {
 	void contextLoads() {
 		int x = 20;
 		int y = 40;
+		int expected = 60;
+		int actual = addTwoNumbers(x,y);
+		Assertions.assertEquals(expected,actual);
+
+		/*if (addTwoNumbers(x,y)==60){
+			System.out.println("As expected");
+		}else{
+			System.out.println("Test failed");
+		}*/
 	}
 
 	private int addTwoNumbers(int x,int y){
